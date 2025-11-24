8d1340ee4e29aeae4047f1ab405b80e5f5b142be
jeffreyboersma <jeffrey.boersma@hotmail.com>
2025-11-24 10:37:09 -0500

    fixed theme issue

--- src/js/theme-init.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    const savedTheme = localStorage.getItem('theme');
-    const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    const currentTheme = savedTheme || systemPreference;
-    document.documentElement.setAttribute('data-theme', currentTheme);
-})();
